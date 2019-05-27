c38787fbf3e056cccc16e727d92ffa5d8185740e
Alexandre Thurow <alexandre@trezo.com.br>
2019-05-27 20:31:22 -0300

    corrigindo aplicação para usar acelerômetro

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-import Ball from "./Ball";
+import { StyleSheet, View, Dimensions } from "react-native";
 
 import { accelerometer, setUpdateIntervalForType } from 'react-native-sensors';
 
-setUpdateIntervalForType("accelerometer", 16);
+setUpdateIntervalForType("accelerometer", 80);
 
 export default class App extends Component {
   constructor(props) {
@@ -12,36 +11,62 @@ export default class App extends Component {
     this.state = {
       acceleration: {
         x: 0,
-        y: 0
+        y: 0,
+        z: 0
       }
     };
   }
 
   componentWillMount() {
-    accelerometer
-      .subscribe(acceleration => this.setState({
-        acceleration,
-      }));
-  }
+    const { width, height } = Dimensions.get('window');
+    this.screenWidth = width;
+    this.screenHeight = height;
+    this.boxWidth = this.screenWidth/10.0
 
+    accelerometer
+    .subscribe(acceleration => this.setState({
+      acceleration,
+    }));
+  }
   render() {
     const { acceleration } = this.state;
 
     return (
-      <View style={styles.table}>
-        <Ball x={acceleration.x} y={acceleration.y}></Ball>
+      <View style={styles.container}>
+        <View style={
+          {
+            position: 'absolute',
+            top: -(this.screenHeight) * (acceleration.y - 1.0)/2.0 - (this.boxWidth/2.0),
+            left: this.screenWidth * (acceleration.x + 1.0)/2.0 - (this.boxWidth/2.0),
+            width: this.screenWidth/20.0,
+            height: this.screenWidth/20.0,
+            borderRadius: 40,
+            backgroundColor: 'red',
+          }
+        } />
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
-  table: {
-    alignItems: "center",
-    backgroundColor: "rgb(50,50,180)",
-    flexDirection: "column",
-    height: 250,
-    justifyContent: "center",
-    width: 250
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  textContainer: {
+    position: 'absolute',
+    top: 40,
   }
 });
